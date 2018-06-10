95277d8ae34d93817b4b8b27540dd9d880f19020
close0ne <turtleland@protonmail.com>
2018-06-11 00:17:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -13,25 +13,25 @@ var networkStat = {
         ["US - turtlepower.challengecoin.io", "https://blocks.turtle.link/pool/turtlepower.challengecoin.io"],
         //EU
         ["EU - eu.turtlepool.space", "https://eu.turtlepool.space/api"],
-        ["EU - trtl.pool.gntl.co.uk", "https://trtl.pool.gntl.co.uk/api"],
         ["EU - trtl.heigh-ho.funkypenguin.co.nz", "https://api.trtl.heigh-ho.funkypenguin.co.nz"],
         ["EU - trtl.cryptohispano.net", "https://trtl.cryptohispano.net/api"],
         ["EU - cryptoknight.cc/turtle", "https://cryptoknight.cc/rpc/turtle"],
         ["EU - trtl.mine2gether.com", "https://trtl.mine2gether.com/api"],
-        ["EU - turtle.coolmining.club", "https://turtle.coolmining.club"],
+      //  ["EU - turtle.coolmining.club", "https://turtle.coolmining.club"],
         ["EU - turtle.mining.garden", "https://turtle.mining.garden:8117"],
         ["EU - trtl.unipool.pro", "https://pool.turtlecoin.fr:8117"],
         ["EU - trtl.ninja", "https://trtl.ninja/api"],
         //Asia
         ["AS - hk.turtlepool.space", "https://hk.turtlepool.space/api"],
         //GEO
-        ["WordWide - turtle.atpool.party", "https://seed.turtle.link/pool/turtle-eu.atpool.party"],
-        ["WordWide - trtl.semipool.com", "https://trtl-api.semipool.com/"],
-        ["WordWide - trtl.cryptopool.space", "https://trtl.cryptopool.space/api"]
+        ["WordWide - turtle.atpool.party", "https://seed.turtle.link/pool/turtle-eu.atpool.party"]
     ]
 };
 var networkStat2 = {
     "trtl": [
-        ["US - turtle.bathmatminingpools.com", "https://api.turtle.bathmatminingpools.com/api"]
+        ["EU - trtl.pool.gntl.co.uk", "https://trtl.pool.gntl.co.uk/api"],
+        ["WordWide - trtl.semipool.com", "https://trtl-api.semipool.com/"],
+        ["turtle.bathmatminingpools.com", "https://api.turtle.bathmatminingpools.com/api"],
+        ["WordWide - trtl.cryptopool.space", "https://trtl.cryptopool.space/api"]
     ]
 };
