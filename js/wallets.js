196a4e31c2246da1d183c9b9b514deab0adc0851
close0ne <turtleland@protonmail.com>
2018-06-14 14:50:45 +0200

    Update wallets.js

--- js/wallets.js
+++ js/wallets.js
@@ -18,4 +18,60 @@ var show_linux = function() {
 
 var show_applegui = function() {
   document.getElementById("applegui").style.display = "block";
+  document.getElementById("applecli").style.display = "none";
+  document.getElementById("wingui").style.display = "none";
+  document.getElementById("wincli").style.display = "none";
+  document.getElementById("linuxgui").style.display = "none";
+  document.getElementById("linuxcli").style.display = "none";
+  
+};
+
+var show_applecli = function() {
+  document.getElementById("applegui").style.display = "none";
+  document.getElementById("applecli").style.display = "block";
+  document.getElementById("wingui").style.display = "none";
+  document.getElementById("wincli").style.display = "none";
+  document.getElementById("linuxgui").style.display = "none";
+  document.getElementById("linuxcli").style.display = "none";
+  
+};
+
+var show_wingui = function() {
+  document.getElementById("applegui").style.display = "none";
+  document.getElementById("applecli").style.display = "none";
+  document.getElementById("wingui").style.display = "block";
+  document.getElementById("wincli").style.display = "none";
+  document.getElementById("linuxgui").style.display = "none";
+  document.getElementById("linuxcli").style.display = "none";
+  
+};
+
+var show_wincli = function() {
+  document.getElementById("applegui").style.display = "none";
+  document.getElementById("applecli").style.display = "none";
+  document.getElementById("wingui").style.display = "none";
+  document.getElementById("wincli").style.display = "block";
+  document.getElementById("linuxgui").style.display = "none";
+  document.getElementById("linuxcli").style.display = "none";
+  
+};
+
+var show_linuxgui = function() {
+  document.getElementById("applegui").style.display = "none";
+  document.getElementById("applecli").style.display = "none";
+  document.getElementById("wingui").style.display = "none";
+  document.getElementById("wincli").style.display = "none";
+  document.getElementById("linuxgui").style.display = "block";
+  document.getElementById("linuxcli").style.display = "none";
+  
+};
+
+var show_linuxcli = function() {
+  document.getElementById("applegui").style.display = "none";
+  document.getElementById("applecli").style.display = "none";
+  document.getElementById("wingui").style.display = "none";
+  document.getElementById("wincli").style.display = "none";
+  document.getElementById("linuxgui").style.display = "none";
+  document.getElementById("linuxcli").style.display = "block";
+  
 };
