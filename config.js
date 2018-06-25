055a72e43b575b0faa1138584ccb1e851cf747d1
close0ne <turtleland@protonmail.com>
2018-06-25 19:16:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -31,6 +31,7 @@ var networkStat2 = {
     "trtl": [
         ["trtl.pool.gntl.co.uk", "https://trtl.pool.gntl.co.uk/api"],
         ["trtl.semipool.com", "https://trtl-api.semipool.com"],
+        ["cryptonote.social", "https://cryptonote.social"],
         ["turtle.bathmatminingpools.com", "https://api.turtle.bathmatminingpools.com/api"],
         ["trtl.cryptopool.space", "https://trtl.cryptopool.space/api"]
     ]
